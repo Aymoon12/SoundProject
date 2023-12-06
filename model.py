db4e0f685f94411343aebb415b397d34ef6bd908
Ayman Anba <aymoonanba45@gmail.com>
2023-12-05 23:50:03 -0500

    added part of Class audio data also added a function that will try to load the data

--- model.py
+++ model.py
@@ -1,2 +1,21 @@
-"Model.py"
+from pydub import AudioSegment
+import numpy as np
+
+
+class AudioData:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.audio = None
+        self.sampling_rate = None
+        self.samples = None
+
+    def load_data(self):
+        try:
+            self.audio = AudioSegment.from_file(self.file_path)
+            self.samples = np.array(self.audio.get_array_of_samples())
+            self.sampling_rate = self.audio.frame_rate
+            return True
+        except Exception as e:
+            print(f"Error loading audio file: {e}")
+            return False
 
--- view.py
+++ view.py
@@ -22,4 +22,3 @@ if __name__ == "__main__":
     create_sine_wave()
 
 
-"THIS IS A TEST"
